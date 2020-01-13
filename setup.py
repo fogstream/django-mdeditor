34a5f407a645e6e09afe2523d7ca5512764a59f3
Dmitry Kotov <dmalexkotov@gmail.com>
2020-01-13 12:38:57 +1000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8'
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='fs-django-mdeditor ',
+    name='fs-django-mdeditor',
     version='0.1.18',
     packages=find_packages(exclude=['mdeditor_demo', 'mdeditor_demo_app.*', 'mdeditor_demo_app']),
     include_package_data=True,
